92ff582079e4e045d9dd38c828cbdcd40f7119f7
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2021-03-02 16:29:57 +0100

    added comment to hello-world.php

--- hello-world.php
+++ hello-world.php
@@ -1,7 +1,11 @@
 <?php
+    // seccsion start which is required in order to use the seccion method
     session_start();
+    // add's first name and last name variables to the seccion
     $_SESSION["fornavn"]="Fjodor";
     $_SESSION["efternavn"]="Dostoyevsky";
+    // lets the user know that the variables are set
     echo "Session variables are set ";
+    // prints out Hello Fjodor Dostoyevsky with the seccion variables
     echo "Hello ".$_SESSION["fornavn"]." ".$_SESSION["efternavn"];
 ?>
